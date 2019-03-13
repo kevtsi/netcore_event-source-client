b88747f42f2613b1c6e30e25a73aebb7eea887f3
kevtsi <24479012+kevtsi@users.noreply.github.com>
2019-03-12 23:57:25 -0500

    Initial check-in of module sse_test

---
+++ sse_test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace sse_test
+{
+    class Program
+    {
+        static WebClient wc;
+        static Uri uri = new Uri("http://localhost:3000/events");
+
+        static void Main(string[] args)
+        {
+            ListenForServerEvents();
+
+            Console.ReadKey();
+        }
+
+        private static void ServerSentEvent(object sender, OpenReadCompletedEventArgs args)
+        {
+            using (var sr = new StreamReader(args.Result))
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            ListenForServerEvents();
+        }
+
+        private static void ListenForServerEvents()
+        {
+            wc = new WebClient();
+            wc.OpenReadAsync(uri);
+            wc.OpenReadCompleted += ServerSentEvent;
+        }
+    }
+}
